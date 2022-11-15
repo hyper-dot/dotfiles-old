91a3b8d97c542578c1966e42c4014cf2fd691e98
Roshan Paudel <rozanpoudel@gmail.com>
2022-11-15 07:18:20 +0545

    Emmet Ls installed and setup lvim for emmet

--- Code - OSS/User/History/-32c597ed/gh2m.jsx
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-
-<style>
-    .hello{
-        
-    }
-</style>
--- Code - OSS/User/History/-55c30ad5/5a6g.jsx
+++
@@ -1,6 +0,0 @@
-import "./post.css";
-const Post = () => {
-  return <div>post</div>;
-};
-
-export default Post;
