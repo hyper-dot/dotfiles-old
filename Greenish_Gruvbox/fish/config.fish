73933b44b5fc26c9e8b0601d47458e722aceb567
Roshan Paudel <rozanpoudel@gmail.com>
2023-01-11 10:25:02 +0545

    Added 3 version of gruvbox dotfiles

---
+++ Greenish_Gruvbox/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    export PATH="/home/linux/.deta/bin:$PATH"
+    export PATH="/home/linux/.local/bin:$PATH"
+end
